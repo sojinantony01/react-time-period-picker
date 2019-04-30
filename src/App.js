31f3662c1d88e66c7e40eba044cd11b14c9f25f5
sojinantony01 <sojin.antony01@gmail.com>
2019-04-30 21:37:35 +0530

    react time range picker

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Time from './time'
+import 'antd/dist/antd.css';
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+       
+      };
+  }
+  onOpeningsChange(e) {
+    this.state.openings = e;
+    this.setState(this.state);
+}
+  render() {
+    return (<div>
+      <Time
+        onChange={this.onOpeningsChange.bind(this)}
+        invalidopenings={this.state.invalidopenings}
+        data={this.state.openings}
+        canOverlap={true}
+        />
+                            
+    </div>)
+  }
+}
+
+export default App;
